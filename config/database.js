741ac73e44cafff7a7a9746cd63aebd504918e01
zia123123 <ziamuhamad@gmail.com>
2021-10-12 20:40:34 +0700

    edit port

--- config/database.js
+++ config/database.js
@@ -3,18 +3,18 @@ require('dotenv').config()
 module.exports = {
 
   // Configuracion de DB
-  // username: process.env.DB_USERNAME || "root",
-  // password: process.env.DB_PASSWORD ||  "",
-  // database: process.env.DB_DATABASE || "evote",
-  // host: process.env.DB_HOST ||"127.0.0.1",
-  // dialect: process.env.DB_DIALECT || "mysql",
-
-
-  username: process.env.DB_USERNAME || "ziakasep",
-  password: process.env.DB_PASSWORD ||  "@Zia123123",
-  database: process.env.DB_DATABASE || "evote",
-  host: process.env.DB_HOST ||"45.13.132.252",
+  username: process.env.DB_USERNAME || "root",
+  password: process.env.DB_PASSWORD ||  "",
+  database: process.env.DB_DATABASE || "ethos",
+  host: process.env.DB_HOST ||"127.0.0.1",
   dialect: process.env.DB_DIALECT || "mysql",
+
+
+  // username: process.env.DB_USERNAME || "ziakasep",
+  // password: process.env.DB_PASSWORD ||  "@Zia123123",
+  // database: process.env.DB_DATABASE || "ethos",
+  // host: process.env.DB_HOST ||"45.13.132.252",
+  // dialect: process.env.DB_DIALECT || "mysql",
 
 
   
