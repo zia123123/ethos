14ce70b9ae9fea9555bc1c4387911dc3f9e0f84b
zia123123 <ziamuhamad@gmail.com>
2022-01-17 20:11:57 +0700

    aziz

--- app/controller/ProductController.js
+++ app/controller/ProductController.js
@@ -60,7 +60,7 @@ module.exports = {
 
     async index(req, res) {
         let result = await products.findAll({
-            attributes: ['id', 'name','expiry_date','price','link','discount','quantity','sku'],
+            attributes: ['weight','id', 'name','expiry_date','price','link','discount','quantity','sku'],
             include: [ 
                 // { model: product_stocks,
                 //     attributes: ['quantity','warehouseId'],
