da4b2cffe5e4131e510dc12133ff7d99bfe490cc
zia123123 <ziamuhamad@gmail.com>
2022-02-07 16:15:08 +0700

    zia

--- app/controller/Ekpedisicontroller.js
+++ app/controller/Ekpedisicontroller.js
@@ -41,9 +41,7 @@ module.exports = {
      //external
      async indexInternal(req, res) {
         let result = await ekpedisi.findAll({
-            where:{
-               internal: true,
-             }
+           
         }).then(result => {
             return apiResponse.successResponseWithData(res, "SUCCESS", result);
             }).catch(function (err){
