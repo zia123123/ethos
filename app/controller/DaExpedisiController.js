1292cc29356d930d2d238050ce08fd0d24ee8793
zia123123 <ziamuhamad@gmail.com>
2022-02-23 20:52:14 +0700

    zia

--- app/controller/DaExpedisiController.js
+++ app/controller/DaExpedisiController.js
@@ -6,31 +6,33 @@ module.exports = {
   
     //create
     async create(req, res) { 
-        let result = await daexpedisis.create({
-            ongkoskirim: req.body.ongkoskirim,
-            subsidi: req.body.subsidi,
-            transaksisId: req.body.transaksiId,
-            namabank: req.body.namabank,
-            totalharga: parseInt(req.body.totalharga),
-            biayatambahan: req.body.biayatambahan,
-            norekening: req.body.norekening ,
-            biayacod: req.body.biayacod,
-        }).then(result => {
-            // let transaksi = transaksis.findOne({
-            //     where: {
-            //         id: req.body.transaksiId
-            //     },
-            // }).then(transaksi =>{
-            //        if(transaksi.pembayaran == 2){
-            //         var harga = parseInt(req.body.totalharga)
-            //        }else{
-            //         var harga = (parseInt(req.body.totalharga) - (parseInt(req.body.transaksiId) % 999));
-            //        }
-            // })
-            return apiResponse.successResponseWithData(res, "SUCCESS CREATE", result);
-        }).catch(function (err)  {
-            return apiResponse.ErrorResponse(res, err);
-        });
+        var harga = 0
+        let transaksi = transaksis.findOne({
+            where: {
+                id: req.body.transaksiId
+            },
+        }).then(transaksi =>{
+               if(transaksi.pembayaran == 2){
+                 harga = parseInt(req.body.totalharga)
+               }else{
+                 harga = (parseInt(req.body.totalharga) - (parseInt(req.body.transaksiId) % 999));
+               }
+               let result =  daexpedisis.create({
+                ongkoskirim: req.body.ongkoskirim,
+                subsidi: req.body.subsidi,
+                transaksisId: req.body.transaksiId,
+                namabank: req.body.namabank,
+                totalharga: harga,
+                biayatambahan: req.body.biayatambahan,
+                norekening: req.body.norekening ,
+                biayacod: req.body.biayacod,
+            }).then(result => {
+                return apiResponse.successResponseWithData(res, "SUCCESS CREATE", result);
+            }).catch(function (err)  {
+                return apiResponse.ErrorResponse(res, err);
+            });
+        })
+       
       },
 
     async index(req, res) {
