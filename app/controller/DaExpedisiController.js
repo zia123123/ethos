eed8604c92fc62d8e11f183c3d48debb4400fdf8
zia123123 <ziamuhamad@gmail.com>
2022-02-23 20:03:45 +0700

    zia

--- app/controller/DaExpedisiController.js
+++ app/controller/DaExpedisiController.js
@@ -6,27 +6,27 @@ module.exports = {
   
     //create
     async create(req, res) { 
-        let transaksi = transaksis.findOne({
-            where: {
-                id: req.body.transaksiId
-            },
-        }).then(transaksi =>{
-               if(transaksi.pembayaran == 2){
-                var harga = parseInt(req.body.totalharga)
-               }else{
-                var harga = (parseInt(req.body.totalharga) - (parseInt(req.body.transaksiId) % 999));
-               }
-        })
         let result = await daexpedisis.create({
             ongkoskirim: req.body.ongkoskirim,
             subsidi: req.body.subsidi,
             transaksisId: req.body.transaksiId,
             namabank: req.body.namabank,
-            totalharga: harga,
+            totalharga: parseInt(req.body.totalharga),
             biayatambahan: req.body.biayatambahan,
             norekening: req.body.norekening ,
             biayacod: req.body.biayacod,
         }).then(result => {
+            // let transaksi = transaksis.findOne({
+            //     where: {
+            //         id: req.body.transaksiId
+            //     },
+            // }).then(transaksi =>{
+            //        if(transaksi.pembayaran == 2){
+            //         var harga = parseInt(req.body.totalharga)
+            //        }else{
+            //         var harga = (parseInt(req.body.totalharga) - (parseInt(req.body.transaksiId) % 999));
+            //        }
+            // })
             return apiResponse.successResponseWithData(res, "SUCCESS CREATE", result);
         }).catch(function (err)  {
             return apiResponse.ErrorResponse(res, err);
