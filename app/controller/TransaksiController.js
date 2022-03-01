8435eb7ce6396ea1e840880683dca36b175eb389
zia123123 <ziamuhamad@gmail.com>
2022-03-01 07:06:42 +0700

    zia

--- app/controller/TransaksiController.js
+++ app/controller/TransaksiController.js
@@ -409,11 +409,11 @@ module.exports = {
                     if(datakeranjang[j] === undefined){
                         KeranjangArray.push(new Keranjang("",""));
                     }else{
-                        KeranjangArray.push(new Keranjang(datakeranjang[j].sku,datakeranjang[j].transaksiId));
+                        KeranjangArray.push(new Keranjang(datakeranjang[j].sku,datakeranjang[j].jumlahproduct));
                     }
                    
                 }               
-                TransaksiArray.push(new Transaksi("FHG",result[i].auth.notelp,result[i].invoiceId,KeranjangArray[0].sku,KeranjangArray[0].jumlahproduct.toString(),KeranjangArray[1].sku,KeranjangArray[1].jumlahproduct.toString(),KeranjangArray[2].sku,KeranjangArray[2].jumlahproduct.toString() ,KeranjangArray[3].sku,KeranjangArray[3].jumlahproduct.toString(),KeranjangArray[4].sku,KeranjangArray[4].jumlahproduct.toString(),result[i].customer.nama,result[i].customer.notelp,result[i].customer.alamat,result[i].awb,result[i].expedisiName,result[i].daexpedisis.totalharga.toString(),result[i].auth.firstname,result[i].warehouse.name,result[i].typebayar.toString(),));
+                TransaksiArray.push(new Transaksi("FHG",result[i].auth.notelp,result[i].invoiceId,KeranjangArray[0].sku,KeranjangArray[0].jumlahproduct.toString(),KeranjangArray[1].sku,KeranjangArray[1].jumlahproduct.toString(),KeranjangArray[2].sku,KeranjangArray[2].jumlahproduct.toString() ,KeranjangArray[3].sku,KeranjangArray[3].jumlahproduct.toString(),KeranjangArray[4].sku,KeranjangArray[4].jumlahproduct.toString(),result[i].customer.nama,result[i].customer.notelp,result[i].customer.alamat,result[i].awb,result[i].expedisiName,result[i].daexpedisis.totalharga.toString(),result[i].auth.firstname,result[i].warehouse.name,result[i].typebayar.toString(),""));
             }
            //console.log(KeranjangArray)
             const wb = new xl.Workbook();
@@ -440,7 +440,9 @@ module.exports = {
                 "COD",
                 "TAG",
                 "Warehouse",
-                "TypeBayar"
+                "TypeBayar",
+                ""
+
             ]
             let headingColumnIndex = 1;
             headingColumnNames.forEach(heading => {
