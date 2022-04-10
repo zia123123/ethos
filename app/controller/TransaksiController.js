b0c11a7f44c56a2e005ce17eab6ab440b050c973
zia123123 <ziamuhamad@gmail.com>
2022-04-11 00:00:43 +0700

    add ajax in transaction

--- app/controller/TransaksiController.js
+++ app/controller/TransaksiController.js
@@ -320,12 +320,9 @@ module.exports = {
 
 
     async riwayatall(req, res) {
-        let page = parseInt(req.query.page)
-        let limit = parseInt(req.query.limit)
-        const count = await transaksis.count()
+       
         let result = await transaksis.findAll({
-            offset: (page - 1) * limit,
-            limit: limit,
+           
             where: {
                 status: {
                     [Op.or]: [
@@ -356,18 +353,7 @@ module.exports = {
                             }
             ]
         }).then(result => {
-            var totalPage = (parseInt(count) / limit) + 1
-            returnData = {
-                result,
-                metadata: {
-                    page: page,
-                    count: result.length,
-                    totalPage: parseInt(totalPage),
-                    totalData:  count,
-                }
-            }
-            
-            return apiResponse.successResponseWithData(res, "SUCCESS", returnData);
+            return apiResponse.successResponseWithData(res, "SUCCESS", result);
             //return apiResponse.successResponseWithData(res, "SUCCESS", result);
             }).catch(function (err){
                 return apiResponse.ErrorResponse(res, err);
@@ -376,7 +362,7 @@ module.exports = {
 
     async ExcelGudang(req, res) {
         let startDate = req.query.startDate+"T00:00:00.000Z"
-        let endDate = req.query.endDate+"T17:00:00.000Z"
+        let endDate = req.query.endDate+"T23:59:00.000Z"
         
 
         let typebayar = req.query.typebayar
