b78b79d7b3ac03d6cb16228520b60097acba5298
Raynaldi <raysnonci@gmail.com>
2022-07-06 14:19:11 +0700

    fixed mutation detail norekening filter

--- app/controller/MutationDetailController.js
+++ app/controller/MutationDetailController.js
@@ -254,7 +254,7 @@ module.exports = {
                 invoice: {
                     [Op.eq]: null
                 },
-                norekening:norekeningFilter,
+                norekeningsId:norekeningFilter,
                 [Op.or]:[
                     {
                         description:{
