eaceaaf0196bc14c3d599d0fd4bcdc13f5596bcd
Raynaldi <raysnonci@gmail.com>
2022-07-12 15:00:35 +0700

    fixed date import cc

--- app/controller/TransaksiController.js
+++ app/controller/TransaksiController.js
@@ -6277,8 +6277,23 @@ module.exports = {
                 }
                 else if (row['WAKTU TTD'] !== undefined) {
                     if (row.STATUS !== undefined) {
+                        var utc_days  = Math.floor(parseFloat(row['WAKTU TTD']) - 25569);
+                        var utc_value = utc_days * 86400;                                        
+                        var date_info = new Date(utc_value * 1000);
+
+                        var fractional_day = row['WAKTU TTD'] - Math.floor(row['WAKTU TTD']) + 0.0000001;
+
+                        var total_seconds = Math.floor(86400 * fractional_day);
+
+                        var seconds = total_seconds % 60;
+
+                        total_seconds -= seconds;
+
+                        var hours = Math.floor(total_seconds / (60 * 60));
+                        var minutes = Math.floor(total_seconds / 60) % 60;
+
                         // sentDate = new Date(tgl.getTime() - (offset*60*1000))
-                        sentDate = new Date(row['WAKTU TTD'])
+                        sentDate = new Date(date_info.getFullYear(), date_info.getMonth(), date_info.getDate(), hours, minutes, seconds);
                         console.log(sentDate);
                     }else{
                         const [date, time] = row['WAKTU TTD'].split('  ')
